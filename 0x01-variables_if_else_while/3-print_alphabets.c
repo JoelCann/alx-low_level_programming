f2a9dcc985fa990a46d70552dd9e29add8f3704c
JoelCann <joelcann12@gmail.com>
2023-03-18 01:59:30 +0000

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - printing alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	char al;
+	char Alpha;
+
+	al = 'a';
+	Alpha = 'A';
+	while (al <= 'z')
+	{
+		putchar(al, Alpha);
+		al++;
+		Alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
