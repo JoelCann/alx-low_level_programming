18146d1c540a96a024df5d902e04ad85f7a5c9fb
JoelCann <joelcann12@gmail.com>
2023-03-21 21:31:16 +0000

    task 0... printing without stdio header

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * alphaprinter - printing alphabets
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	void alpha_printer(void)
+	{
+		int alph;
+		for (alph = 'a' ; alph <= 'z' alph ++)
+		{
+			_putchar('alph');
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
