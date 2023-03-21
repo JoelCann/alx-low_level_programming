c7cd1403abeef6cb501142b0a15e0334c2ce8898
JoelCann <joelcann12@gmail.com>
2023-03-21 22:04:21 +0000

    task 2... printing 10 alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,21 @@
 #include "main.h"
 /**
- * main - check the code.
- * Return: Always 0.
+ * print_alphabet_x10 - print alphabets
+ * Return: void
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-    print_alphabet_x10();
-    return (0);
+	char al;
+
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (al = 'a'; al <= 'z'; al++)
+		{
+			_putchar(al);
+		}
+		_putchar('\n');
+		i++;
+	}
 }
