e8181b9df2841f369b40bb0f58dbefa700e48171
JoelCann <joelcann12@gmail.com>
2023-04-13 17:56:56 +0000

    more on mallock task 2 completed

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
-* _calloc - allocates memory for an array of @nmemb elements of
-* @size bytes each and returns a pointer to the allocated memory.
+* _calloc - allocates memory for an array
 *
-* @nmemb: allocate memory for array
-* @size: allocate element of size bytes
+* @nmemb: number of elements in array
+* @size: size(in bytes) of each element in array
 *
 * Return: pointer to the allocated memory.
 */
@@ -18,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	a = malloc(nmemb * size);
 	if (a == NULL)
 		return (NULL);
-	for (b = 0; b < (nmemb * size); b++)
+	for (b = 0; b < (nmemb * size); b += 1)
 		a[b] = 0;
 	return (a);
 }
