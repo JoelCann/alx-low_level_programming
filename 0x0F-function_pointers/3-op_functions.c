a16563a8641c30e8011d258edf6c1b6e4b846c99
JoelCann <joelcann12@gmail.com>
2023-04-20 14:15:03 +0000

    task 3 corrections

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,60 +2,60 @@
 
 /**
  * op_add - sums up two integers
- * @x: int one
- * @y: int two
+ * @a: int one
+ * @b: int two
  *
  * Return: total
  */
-int op_add(int x, int y)
+int op_add(int a, int b)
 {
-	return (x + y);
+	return (a + b);
 }
 
 /**
  * op_sub - difference of two integers.
- * @x: int one
- * @y: int two
+ * @a: int one
+ * @b: int two
  *
  * Return: total difference
  */
-int op_sub(int x, int y)
+int op_sub(int a, int b)
 {
-	return (x - y);
+	return (a - b);
 }
 
 /**
  * op_mul - mulplies an integer by another
- * @x: int one
- * @y: int two
+ * @a: int one
+ * @b: int two
  *
  * Return: total product
  */
-int op_mul(int x, int y)
+int op_mul(int a, int b)
 {
-	return (x * y);
+	return (a * b);
 }
 
 /**
  * op_div - divides an integer by another
- * @x: int one
- * @y: int two
+ * @a: int one
+ * @b: int two
  *
  * Return: final quotient.
  */
-int op_div(int x, int y)
+int op_div(int a, int b)
 {
-	return (x / y);
+	return (a / b);
 }
 
 /**
  * op_mod - modulo of two integers
- * @x: int one
- * @y: int two
+ * @a: int one
+ * @b: int two
  *
  * Return: the remainder
  */
-int op_mod(int x, int y)
+int op_mod(int a, int b)
 {
-	return (x % y);
+	return (a % b);
 }
