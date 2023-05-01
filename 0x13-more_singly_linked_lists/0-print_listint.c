23c89a5e7db4e8fe74df326a66bc44d2134d27c7
JoelCann <joelcann12@gmail.com>
2023-05-01 11:17:25 +0000

    made corrections to task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * print_listint - Prints out all elements in the listint_t list.
- * @h: Points to the head of the listint_t list.
+ * @y: Points to the head of the listint_t list.
  * Return: The total no. of nodes in the listint_t list.
  */
 size_t print_listint(const listint_t *y)
