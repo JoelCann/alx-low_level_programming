c326b28f321a41d7346882fe1601465faa60f52f
JoelCann <joelcann12@gmail.com>
2023-05-01 16:04:15 +0000

    completed task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,40 +1,47 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes a node in a linked list at a certain index
- * @head: pointer to the first element in the list
- * @index: index of the node to delete
+ * delete_nodeint_at_index - deletes a node from a particular
+ * index in a linked list
+ * @head: points to the first object in a linked list.
+ * @idx: index of the node to be deleted.
  *
- * Return: 1 (Success), or -1 (Fail)
+ *insert_nodeint_at_index - inserts node into particular index
+ *
+ * Return: 1 if successful or -1 if failed
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	if (head == NULL)
+		return (NULL);
+
+	listint_t *new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	listint_t *temp = *head;
-	listint_t *current = NULL;
-	unsigned int i = 0;
 
-	if (*head == NULL)
-		return (-1);
-
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		free(temp);
-		return (1);
-	}
-
-	while (i < index - 1)
-	{
-		if (!temp || !(temp->next))
-			return (-1);
+	for (unsigned int i = 0; i < idx - 1 && temp != NULL; i++)
 		temp = temp->next;
-		i++;
+
+	if (temp == NULL)
+	{
+		free(new);
+		return (NULL);
 	}
 
+	new->next = temp->next;
+	temp->next = new;
 
-	current = temp->next;
-	temp->next = current->next;
-	free(current);
-
-	return (1);
+	return (new);
 }
