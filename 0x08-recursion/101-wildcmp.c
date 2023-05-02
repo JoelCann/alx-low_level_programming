eadbf1b1becb7f556bf10398d498be0b90ba08fc
JoelCann <joelcann12@gmail.com>
2023-05-02 15:36:12 +0000

    finishing additional tasks 7 and 8 on recurssion project

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,15 +6,17 @@
  * @s2: pointer to string params
  * Return: 0
  */
-int wildcmp(const char *s1, const char *s2)
+switch (*s1)
 {
-	switch (*s1)
-	{
-		case '\0':
-			return (*s2 == '\0');
-		case '*':
-			return (wildcmp(s1 + 1, s2) | wildcmp(s1, s2 + 1));
-		default:
-			return (*s1 == *s2 ? wildcmp(s1 + 1, s2 + 1) : 0);
-	}
+    case '\0':
+        return (*s2 == '\0');
+    case '*':
+        return (wildcmp(s1 + 1, s2) | wildcmp(s1, s2 + 1));
+    default:
+        if (*s1 == *s2)
+        {
+            return (wildcmp(s1 + 1, s2 + 1));
+        }
+        return (0);
 }
+
