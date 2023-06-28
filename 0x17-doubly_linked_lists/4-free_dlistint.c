a7395cc5e6b43d11787a221e6886ed36a90c4a75
JoelCann <joelcann12@gmail.com>
2023-06-28 17:41:34 +0000

    task 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -5,16 +5,16 @@
  * free_dlistint - frees a dlistint_t list
  * @head: points to start of linked list
  *
- * Return: void
+ * Return: nothing
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *nxt;
+	dlistint_t *next;
 
 	while (head != NULL)
 	{
-		nxt = head->nxt;
+		next = head->next;
 		free(head);
-		head = nxt;
+		head = next;
 	}
 }
