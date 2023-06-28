746517997d6c34d600f057431273966f1c8c6178
JoelCann <joelcann12@gmail.com>
2023-06-28 17:55:59 +0000

    task 6,7

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - sums all of the data of a dlistint_t linked list
- * @head: pointer to the beginning of the linked list
+ * sum_dlistint - adds all data of a dlistint_t linked list
+ * @head: points to start of the linked list
  *
- * Return: sum of all data, or 0 if the list is empty
+ * Return: sum(data)/ 0 if list is empty
  */
 int sum_dlistint(dlistint_t *head)
 {
-	int sum = 0;
+	int tot = 0;
 
 	while (head != NULL)
 	{
-		sum += head->n;
+		tot += head->n;
 		head = head->next;
 	}
-	return (sum);
+	return (tot);
 }
--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,26 +3,26 @@
 #include <stdio.h>
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position
+ * insert_dnodeint_at_index - adds a new node at a given position
  * @h: double pointer to the beginning of the linked list
- * @idx: index at which to insert the new node
- * @n: data to enter into new node
+ * @idx: index/point to insert the new node
+ * @n: data to be entered into new node
  *
- * Return: pointer to the new node, or NULL on failure
+ * Return: points to new node/ NULL on failure
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *next, *current;
-	unsigned int i;
+	dlistint_t *new, *next, *crnt;
+	unsigned int a;
 
 	if (h == NULL)
 		return (NULL);
 	if (idx != 0)
 	{
-		current = *h;
-		for (i = 0; i < idx - 1 && current != NULL; i++)
-			current = current->next;
-		if (current == NULL)
+		crnt = *h;
+		for (a = 0; a < idx - 1 && crnt != NULL; a += 1)
+			crnt = crnt->next;
+		if (crnt == NULL)
 			return (NULL);
 	}
 	new = malloc(sizeof(dlistint_t));
@@ -33,16 +33,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		next = *h;
 		*h = new;
-		new->prev = NULL;
+		new->prv = NULL;
 	}
 	else
 	{
-		new->prev = current;
-		next = current->next;
-		current->next = new;
+		new->prv = crnt;
+		next = crnt->next;
+		crnt->next = new;
 	}
 	new->next = next;
 	if (new->next != NULL)
-		new->next->prev = new;
+		new->next->prv = new;
 	return (new);
 }
